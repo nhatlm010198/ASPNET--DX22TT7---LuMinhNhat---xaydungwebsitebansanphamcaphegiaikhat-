1d3ce6c63665130c51be13f6b0c48036a98a34d1
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:10:07 +0700

    Add OrderDetail model with foreign keys

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -1,1 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeShopWebsite.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+
+        [ForeignKey("Order")]
+        public int OrderId { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
 

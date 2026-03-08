9b89d086f27dff2a300ac14458d331d5c643f670
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:11:25 +0700

    Add ProductController with Index and Details actions

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,1 +1,26 @@
+using CoffeeShopWebsite.Models;
+using System.Linq;
+using System.Web.Mvc;
 
+namespace CoffeeShopWebsite.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeDbContext db = new CoffeeDbContext();
+
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            var products = db.Products.Include("Category").ToList();
+            return View(products);
+        }
+
+        // GET: /Product/Details/5
+        public ActionResult Details(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product == null) return HttpNotFound();
+            return View(product);
+        }
+    }
+}

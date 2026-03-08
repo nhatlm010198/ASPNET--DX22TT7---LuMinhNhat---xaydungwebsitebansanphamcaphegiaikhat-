73afcd55f306ede05da97bdd337c598993552703
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:09:05 +0700

    Update Category.cs

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,1 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
+namespace CoffeeShopWebsite.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}

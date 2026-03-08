46a5594295b8f90346d845a5e060dc0eb4b55f66
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:12:36 +0700

    Update AdminController.cs

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,1 +1,54 @@
+using CoffeeShopWebsite.Models;
+using System.Linq;
+using System.Web.Mvc;
 
+namespace CoffeeShopWebsite.Controllers
+{
+    public class AdminController : Controller
+    {
+        private CoffeeDbContext db = new CoffeeDbContext();
+
+        private bool IsAdmin()
+        {
+            return Session["Role"] != null && Session["Role"].ToString() == "Admin";
+        }
+
+        public ActionResult Index()
+        {
+            if (!IsAdmin()) return RedirectToAction("Login", "Account");
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            if (!IsAdmin()) return RedirectToAction("Login", "Account");
+            var products = db.Products.ToList();
+            return View(products);
+        }
+
+        public ActionResult CreateProduct() => View();
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Products");
+            }
+            return View(product);
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = db.Products.Find(id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Products");
+        }
+    }
+}

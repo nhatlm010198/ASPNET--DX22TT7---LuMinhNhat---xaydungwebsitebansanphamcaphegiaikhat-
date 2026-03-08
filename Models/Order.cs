42a68bffa448b4296c097e64ae13d3273e5694bc
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:09:49 +0700

    Add Order model with properties and relationships

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
 
+namespace CoffeeShopWebsite.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public string Status { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}

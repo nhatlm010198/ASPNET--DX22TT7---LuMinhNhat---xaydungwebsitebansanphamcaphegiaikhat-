64445489b77e4b70d7f70f489dd5ca4d2a1eec0e
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:28:42 +0700

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 
+namespace CoffeeShopWebsite
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Đăng ký tất cả các Area
+            AreaRegistration.RegisterAllAreas();
+
+            // Đăng ký Route
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}

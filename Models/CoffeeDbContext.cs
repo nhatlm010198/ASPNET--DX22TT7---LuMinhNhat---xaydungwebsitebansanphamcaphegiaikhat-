08bd76d3435a0040a0ab32ac5658aadd6bca28b2
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:10:27 +0700

    Update CoffeeDbContext.cs

--- Models/CoffeeDbContext.cs
+++ Models/CoffeeDbContext.cs
@@ -1,1 +1,15 @@
+using System.Data.Entity;
 
+namespace CoffeeShopWebsite.Models
+{
+    public class CoffeeDbContext : DbContext
+    {
+        public CoffeeDbContext() : base("CoffeeDbConnection") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}

54349d5dbf769645b2f13f07af65b8f9984a1c86
nhatlm010198 <nhatlm010198@tvu-onschool.edu.vn>
2026-03-08 20:08:38 +0700

    Define User class and UserRole enum
    Added User class with properties and UserRole enum.

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,31 @@
-public enum UserRole
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopWebsite.Models
 {
-    Customer = 0,  // Khách hàng
-    Staff = 1,     // Nhân viên
-    Admin = 2      // Quản trị viên
+    public enum UserRole
+    {
+        Customer = 0,
+        Staff = 1,
+        Admin = 2
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        public UserRole Role { get; set; }
+    }
 }
